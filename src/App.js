8e2900bc204514d631914a46ce3291f41de60205
John Tucker <john@larkintuckerllc.com>
2020-08-18 22:18:55 -0400

    flow

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import React, { useCallback, useEffect, useState } from 'react';
+import { getAuthenticated, loadLoginScreen, login, logout, validateState } from './api/oidc';
+
+const params = (new URL(document.location)).searchParams;
+const state = params.get('state');
+const code = params.get('code'); 
+const auth = getAuthenticated();
+
+export default function App() {
+  const [authenticated, setAuthenticated] = useState(auth);
+  const [authenticating, setAuthenticating] = useState(code !== null);
+  useEffect(() => {
+    const execute = async () => {
+      window.history.replaceState({}, document.title, '/');
+      try {
+        const validState = validateState(state);
+        if (!validState) throw new Error();
+        await login(code);
+        setAuthenticated(true);
+      } catch (err) {
+        // DO NOTHING
+      }
+      setAuthenticating(false);
+    };
+    if (code !== null) {
+      execute();
+    }
+  }, []);
+  const handleLoginClick = useCallback(() => {
+    loadLoginScreen();
+  }, []);
+  const handleLogoutClick = useCallback(() => {
+    logout();
+    setAuthenticated(false);
+  }, []);
+
+  if (authenticating) {
+    return <div>authenticating...</div>;
+  }
+  if (!authenticated) {
+    return <button onClick={handleLoginClick}>Login</button>;
+  }
+  return <button onClick={handleLogoutClick}>Logout</button>
+}
--- src/api/oidc.js
+++ src/api/oidc.js
@@ -1,16 +1,47 @@
+import jwt_decode from 'jwt-decode';
 import { v4 as uuidv4 } from 'uuid';
 
-export function getAuthenticated() {
-    const idToken = window.localStorage.getItem('id_token');
-    return idToken !== null;
+export const getAuthenticated = () => {
+  const idToken = window.localStorage.getItem('id_token');
+  return idToken !== null;
 }
 
-export function login() {
-    window.localStorage.removeItem('id_token');
-    window.localStorage.removeItem('refresh_token');
-    const state = uuidv4();
-    const nonce = uuidv4();
-    window.localStorage.setItem('state', state);
-    window.localStorage.setItem('nonce', nonce);
-    window.location.assign(`${process.env.REACT_APP_OIDC_URI_BASE}/login?state=${state}&nonce=${nonce}`);
+export const loadLoginScreen = () => {
+  window.localStorage.removeItem('id_token');
+  window.localStorage.removeItem('refresh_token');
+  const state = uuidv4();
+  const nonce = uuidv4();
+  window.localStorage.setItem('state', state);
+  window.localStorage.setItem('nonce', nonce);
+  window.location.assign(`${process.env.REACT_APP_OIDC_URI_BASE}/login-screen?state=${state}&nonce=${nonce}`);
+}
+
+export const validateState = checkState => {
+  const state = window.localStorage.getItem('state');
+  window.localStorage.removeItem('state');
+  return checkState === state;
+}
+
+const validateNonce = checkNonce => {
+  const nonce = window.localStorage.getItem('nonce');
+  window.localStorage.removeItem('nonce');
+  return checkNonce === nonce;
+}
+
+export const login = async code => {
+  const response = await fetch(`${process.env.REACT_APP_OIDC_URI_BASE}/get-tokens?code=${code}`);
+  if (!response.ok) {
+    throw new Error();
+  }
+  const { id_token, refresh_token } = await response.json();
+  const { nonce }= jwt_decode(id_token);
+  const validNonce = validateNonce(nonce);
+  if (!validNonce) throw new Error();
+  window.localStorage.setItem('id_token', id_token);
+  window.localStorage.setItem('refresh_token', refresh_token);
+}
+
+export const logout = () => {
+  window.localStorage.removeItem('id_token');
+  window.localStorage.removeItem('refresh_token');
 }
